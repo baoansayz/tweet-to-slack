5c93f199435dc60d3a83b77a692405bc33bdd268
Baoan Nguyen <baoansayz@gmail.com>
2017-08-10 04:39:33 -0500

    formatting

--- src/Slackbot.java
+++ src/Slackbot.java
@@ -22,14 +22,14 @@ public class Slackbot{
 	    
 		//config to access twitter API
 		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-	    configurationBuilder.setOAuthConsumerKey("6j1GwRT84MR3ywGVqWgbN9N3P")
-	                			.setOAuthConsumerSecret("829IlFyUe1m1NLGIemfh7CVY2BnmCd46nzLFDWcbxA5zOMIiQs")
+		configurationBuilder	.setOAuthConsumerKey("6j1GwRT84MR3ywGVqWgbN9N3P")
+	                		    .setOAuthConsumerSecret("829IlFyUe1m1NLGIemfh7CVY2BnmCd46nzLFDWcbxA5zOMIiQs")
 	                			.setOAuthAccessToken("82011425-4OZAy81bHMQ0IZMckFutcF9uWiKcIrJGpWUdhO4L5")
 	                			.setOAuthAccessTokenSecret("iPU3tYhAMal6hLQGTabkVruwnhBBrGud41zMDInck4wFd");
 
 		//establish connection to twiiter
-		TwitterStream twitterStream = new TwitterStreamFactory(configurationBuilder.build())
-									 .getInstance();
+		TwitterStream twitterStream 		= 	new TwitterStreamFactory(configurationBuilder.build())
+									 		.getInstance();
 		
 		StatusListener statusListener = new StatusListener() {
 	    @Override
